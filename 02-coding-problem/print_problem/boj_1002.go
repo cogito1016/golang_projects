7608dd102c3ccf3aa39500aaaaa0590cbbe13659
김재형 <cogito1016@naver.com>
2023-08-03 22:16:52 +0900

    feat: boj_1002

--- 02-coding-problem/print_problem/boj_1002.go
+++ 02-coding-problem/print_problem/boj_1002.go
@@ -3,5 +3,12 @@ package print_problem
 import "fmt"
 
 func RunBoj1002(){
-	fmt.Println("this is boj1002");
+	//3개의 정수를 10 11 12 처럼 한줄로 입력받는 방법
+	var a, b, c int;
+	fmt.Scan(&a, &b, &c);
+
+	var result int;
+	result = a + b + c;
+
+	fmt.Println(result);
 }
