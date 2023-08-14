65ffb39421cbf7dd468ae2f6abdd80af9d85cf66
김재형 <cogito1016@naver.com>
2023-08-15 01:28:29 +0900

    refactor: boj_25206 IndexOutofBounds 개선, scanner을 밖으로뺴고 ParseFloat은 64로 pointSum과 맞춰야할듯

--- 02-coding-problem/string_problem/boj_25206.go
+++ 02-coding-problem/string_problem/boj_25206.go
@@ -9,49 +9,48 @@ import (
 )
 
 func RunBoj25206() {
-
 	pointSum := 0.0
 	pointAndGradeSum := 0.0
+	scanner := bufio.NewScanner(os.Stdin)
 
 	for i := 0; i < 20; i++ {
-		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
 		input := strings.Fields(scanner.Text())
-		point, _ := strconv.ParseFloat(input[1], 32)
+		point, _ := strconv.ParseFloat(input[1], 64)
 		grade := input[2]
 		if grade == "P" {
 			continue
 		}
 
 		pointSum += point
+
 		pointAndGradeSum += point * getPointByGrade(grade)
 	}
 
-	fmt.Printf("%f", pointAndGradeSum/pointSum)
-
+	fmt.Printf("%.6f", pointAndGradeSum/pointSum)
 }
+
 func getPointByGrade(grade string) float64 {
-	eng := grade[0]
 	var point float64
 
-	if eng == 'F' {
+	if grade == "F" {
 		return 0.0
-	} else if eng == 'A' {
+	} else if grade == "A0" {
 		point = 4.0
-	} else if eng == 'B' {
+	} else if grade == "B0" {
 		point = 3.0
-	} else if eng == 'C' {
+	} else if grade == "C0" {
 		point = 2.0
-	} else {
+	} else if grade == "D0" {
 		point = 1.0
-	}
-
-	if len(grade) == 2 {
-		weight := grade[1]
-
-		if weight == '+' {
-			point += 0.5
-		}
+	} else if grade == "A+" {
+		point = 4.5
+	} else if grade == "B+" {
+		point = 3.5
+	} else if grade == "C+" {
+		point = 2.5
+	} else if grade == "D+" {
+		point = 1.5
 	}
 
 	return point
