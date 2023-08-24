5923509ef5710848e7b388d9640055499697d236
김재형 <cogito1016@naver.com>
2023-08-25 05:07:46 +0900

    refactor: 코드포메팅

--- 02-coding-problem/recursive_problem/boj_1074.go
+++ 02-coding-problem/recursive_problem/boj_1074.go
@@ -9,11 +9,6 @@ import (
 )
 
 // Z
-
-var targetY int
-var targetX int
-var result int
-
 func RunBoj1074() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
@@ -21,9 +16,6 @@ func RunBoj1074() {
 	N, _ := strconv.Atoi(input[0])
 	r, _ := strconv.Atoi(input[1])
 	c, _ := strconv.Atoi(input[2])
-	targetY = r
-	targetX = c
-	result = 0
 
 	result := zOrderTraversal(N, r, c)
 	fmt.Println(result)
@@ -55,34 +47,34 @@ func zOrderTraversal(N, r, c int) int {
 	return result
 }
 
-//정석적인방법, 그러나 최악의경우 시간초과가 발생한다.
-func z_recursive(n int, y int, x int) {
-	if n == 2 {
-		if y == targetY && x == targetX {
-			fmt.Println(result)
-			return
-		}
-		result++
-		if y == targetY && x+1 == targetX {
-			fmt.Println(result)
-			return
-		}
-		result++
-		if y+1 == targetY && x == targetX {
-			fmt.Println(result)
-			return
-		}
-		result++
-		if y+1 == targetY && x+1 == targetX {
-			fmt.Println(result)
-			return
-		}
-		result++
-		return
-	}
-
-	z_recursive(n/2, y, x)
-	z_recursive(n/2, y, x+n/2)
-	z_recursive(n/2, y+n/2, x)
-	z_recursive(n/2, y+n/2, x+n/2)
-//}
+////정석적인방법, 그러나 최악의경우 시간초과가 발생한다.
+//func z_recursive(n int, y int, x int) {
+//	if n == 2 {
+//		if y == targetY && x == targetX {
+//			fmt.Println(result)
+//			return
+//		}
+//		result++
+//		if y == targetY && x+1 == targetX {
+//			fmt.Println(result)
+//			return
+//		}
+//		result++
+//		if y+1 == targetY && x == targetX {
+//			fmt.Println(result)
+//			return
+//		}
+//		result++
+//		if y+1 == targetY && x+1 == targetX {
+//			fmt.Println(result)
+//			return
+//		}
+//		result++
+//		return
+//	}
+//
+//	z_recursive(n/2, y, x)
+//	z_recursive(n/2, y, x+n/2)
+//	z_recursive(n/2, y+n/2, x)
+//	z_recursive(n/2, y+n/2, x+n/2)
+////}
