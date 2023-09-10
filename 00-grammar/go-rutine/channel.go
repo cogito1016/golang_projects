05567d52e3b18838dc427d0934d7862552e3cddf
김재형 <cogito1016@naver.com>
2023-09-10 19:27:21 +0900

    feat: 채널, 버퍼를사용하지않는 채널, 닫힌채널에서 값을 가져오면?

--- 00-grammar/go-rutine/channel.go
+++ 00-grammar/go-rutine/channel.go
@@ -6,9 +6,10 @@ import (
 )
 
 func RunChannelBasic() {
-	c := make(chan int, 1)
-
 	var waitGroup sync.WaitGroup
+
+	//버퍼를 사용하는 채널
+	c := make(chan int, 1)
 	waitGroup.Add(1)
 	go func(c chan int) {
 		defer waitGroup.Done()
@@ -17,6 +18,32 @@ func RunChannelBasic() {
 	}(c)
 
 	fmt.Println("Read:,", <-c)
+
+	waitGroup.Wait()
+
+	//버퍼를사용하지않는 채널
+	ch := make(chan bool)
+	for i := 0; i < 5; i++ {
+		go printer(ch)
+	}
+
+	n := 0
+	for i := range ch {
+		fmt.Println(i)
+		if i == true {
+			n++
+		}
+
+		if n > 2 {
+			fmt.Println("n: ", n)
+			close(ch) //채널을닫는다
+			break
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-ch) //falese 가 5번 출력된다, 이유? 닫힌채널에서 데이터를 가져오려면, 채널타입의 제로값을 가져온다.
+	}
 }
 
 func writeToChannel(c chan int, x int) {
