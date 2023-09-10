90767b31d1d4c6f1fa8af66db2e13bad16e04706
김재형 <cogito1016@naver.com>
2023-09-10 18:16:53 +0900

    feat: Go Rutine, 고루틴들은 순서를 보장하지않는다, Go프로그램은 Go루틴의 종료를 기다리지않는다

--- 00-grammar/go-rutine/go-rutine.go
+++ 00-grammar/go-rutine/go-rutine.go
@@ -3,14 +3,43 @@ package go_rutine
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func Run() {
-	MyInfo()
+	//MyInfo()
+	//GoRutinesInfo()
+
+	//고루틴의 순서는 무작위
+	//go TestGoRutine(10)
+	//go Printme(15)
+	//LazyDone()
+
+	for i := 0; i < 10; i++ {
+		go func(x int) { fmt.Printf("%d ", x) }(i)
+	} //0 9 4 7 8 5 3 1 2 6 Exiting...
+	LazyDone()
 }
 
 func MyInfo() {
 	fmt.Println("You are using ", runtime.Compiler)    //gc
 	fmt.Println("on a ", runtime.GOARCH, "machine")    // arm64
 	fmt.Println("Using Go version", runtime.Version()) //1.20.6
+}
+
+func GoRutinesInfo() {
+	fmt.Printf("GOMAXPROCS : %d\n", runtime.GOMAXPROCS(0)) //인자가 0이면 현재값, 다른수면 변경
+}
+
+func TestGoRutine(x int) {
+	fmt.Printf("%d ", x)
+}
+
+func Printme(x int) {
+	fmt.Printf("%d", x)
+}
+
+func LazyDone() { //Go프로그램은 Go루틴이 끝날때까지 기다리지않기때문에 출력문을 확인하기위해 일부러 지연시키는 것
+	time.Sleep(time.Second)
+	fmt.Println("Exiting...")
 }
