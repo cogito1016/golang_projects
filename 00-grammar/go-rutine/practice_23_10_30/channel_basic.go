6301ad5b6a9941682d97836e9f69980fe3b78448
김재형 <cogito1016@naver.com>
2023-10-31 00:07:54 +0900

    refactor: 생산자, 소비자 구현

--- 00-grammar/go-rutine/practice_23_10_30/channel_basic.go
+++ 00-grammar/go-rutine/practice_23_10_30/channel_basic.go
@@ -12,20 +12,24 @@ var wg sync.WaitGroup
 func RunChannelBasic() {
 	ch := make(chan int)
 
+	wg.Add(1)
 	go func(c chan<- int) {
-		time.Sleep(time.Second)
-		wg.Add(1)
-		c <- rand.Intn(10)
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Second)
+			c <- rand.Intn(10)
+		}
+		c = nil //의도적으로 블록시킨다, close를 할 시 select input := <-c에서 제로값을 무수히많이 받아버림
 	}(ch)
 
 	go func(c chan int) {
 		for {
 			select {
-			case <-c:
-				fmt.Println(<-c)
-				wg.Done()
-			case <-time.After(5 * time.Second):
+			case input := <-c:
+				fmt.Println(input)
+			case <-time.After(10 * time.Second):
 				fmt.Println("Time over ")
+				wg.Done()
+				return
 			}
 		}
 	}(ch)
